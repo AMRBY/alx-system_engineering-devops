25ab1e442d5590196a6543f4aecb840a9ea168cb
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-22 16:12:56 +0000

    mandatory

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,3 +5,8 @@ ensure   => '2.1.0'
 provider => 'pip3'
 require  => 'Package['python3-pip']'
 }
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
