a6e05d5414481bee9fbf99337065a80becf75e09
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-22 16:16:00 +0000

    mandatory

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-# install Flask v2.1.0  using pip3
+# Puppet manifest to install Flask
 
 package { 'python3-pip':
   ensure => installed,
@@ -9,10 +9,3 @@ package { 'flask':
   provider => 'pip3',
   require  => Package['python3-pip'],
 }
-
-package { 'werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip',
-  require  => Package['python3-pip'],
-}
-
