c344ad4ff18fa5eeb9fa23376c176614e696b124
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-22 17:30:18 +0000

    mandatory

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # Puppet manifest to kill a process
 
 exec { 'killmenow':
-command  => 'kill -9 killmenow',
+command  => 'pkill killmenow',
 provider => 'shell'
-onlyif   => 'ps -ef |grep killmenow'
 }
