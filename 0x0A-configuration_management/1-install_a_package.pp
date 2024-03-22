cffc4dd1dbd5a30fc725343d09ae0b5e9484b8b9
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2024-03-22 16:07:22 +0000

    mandatory

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,13 @@
 # Puppet manifest to install package 'flask'
 
+package {'python':
+ensure   => installed
+}
 package {'flask':
 ensure   => '2.1.0'
-name     => 'flask'
+provider => 'pip3'
+}
+package {'werkzeug':
+ensure   => '2.1.1'
 provider => 'pip3'
 }
