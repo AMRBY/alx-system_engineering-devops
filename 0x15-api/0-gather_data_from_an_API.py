9e6cded6de0713319eaa9756e0cba81807fb982b
AMR BY <bouyoussefamr@gmail.com>
2024-04-25 09:40:44 +0000

    mandatory

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,16 +6,19 @@ from sys import argv
 if __name__ == "__main__":
     number = 0
     done = []
+
     user = requests.get(
             f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
     response = requests.get(
             f'https://jsonplaceholder.typicode.com/todos/?userId={argv[1]}')
-    # print(response.json())
+
     total = len(response.json())
     for i in range(len(response.json())):
         if response.json()[i]['completed'] is True:
             number += 1
             done.append(response.json()[i]['title'])
+
     print(f'Employee {user.json()["name"]} \
-is done with tasks ({number}/{total}):')
-    print(*done, sep="\n")
+is done with tasks({number}/{total}):')
+    for item in done:
+        print("\t " + item)
