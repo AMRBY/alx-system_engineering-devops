c70c66ad3f1ad31b996b5bd09e4eaf7b81d718ad
AMR BY <bouyoussefamr@gmail.com>
2024-04-25 11:01:50 +0000

    mandatory

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" this is an API """
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    di = {}
+    js = {}
+
+    user = requests.get(
+            f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    response = requests.get(
+            f'https://jsonplaceholder.typicode.com/todos/?userId={argv[1]}')
+
+    file_path = argv[1] + '.json'
+    with open(file_path, 'w', newline="") as f:
+
+        for i in range(len(response.json())):
+        #print(response.json()[i]['title'], response.json()[i]['completed'], user.json()['username'])
+            di["task"] = response.json()[i]['title']
+            di["completed"] = response.json()[i]['completed']
+            di["username"] = user.json()['username']
+            js[argv[1]] = di
+            json.dump(js, f)
+            print(js)
