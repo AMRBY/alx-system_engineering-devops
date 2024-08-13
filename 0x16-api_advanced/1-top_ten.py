091a05acdc82c7899ccc5022cacc9862dd15e26a
AMRBY <bouyoussefamr@gmail.com>
2024-08-13 14:54:05 +0000

    mandatory

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,12 +5,13 @@ import requests
 
 def top_ten(subreddit):
     """Top ten hot posts"""
-    base_url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
+    base_url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'user-agent': 'amrby'}
     response = requests.get(base_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        for i in response.json()["data"]['children']:
+        lista = response.json()["data"]['children']
+        for i in lista[:10]:
             print(f">>>{i['data']['title']}")
     else:
         return 0
