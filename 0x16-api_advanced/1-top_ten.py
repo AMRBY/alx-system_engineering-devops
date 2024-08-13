f41b359e9f7c6aa6312bf672d5bc428002590c82
AMRBY <bouyoussefamr@gmail.com>
2024-08-13 14:51:07 +0000

    mandatory

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,12 +4,13 @@ import requests
 
 
 def top_ten(subreddit):
-    """ number of subscribers"""
-    base_url = 'https://www.reddit.com/r/{}/hot.json?limit=2'.format(subreddit)
-    headers = {'User-Agent': 'alx_agent'}
+    """Top ten hot posts"""
+    base_url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
+    headers = {'user-agent': 'amrby'}
     response = requests.get(base_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        print(response.json()["data"]['children'])
+        for i in response.json()["data"]['children']:
+            print(f">>>{i['data']['title']}")
     else:
         return 0
