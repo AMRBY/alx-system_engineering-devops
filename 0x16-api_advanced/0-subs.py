fc6109f7cc400608989c10555f7b32060aecf52a
AMRBY <bouyoussefamr@gmail.com>
2024-08-13 13:59:35 +0000

    mandatory

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
     """ number of subscribers"""
     base_url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'alx_agent'}
-    response = requests.get(base_url, headers=headers, allow_redirects=False)
+    client = requests.session()
+    client.headers = headers
+    response = requests.get(base_url, allow_redirects=False)
 
     if response.status_code == 200:
         return (response.json()["data"]["subscribers"])
