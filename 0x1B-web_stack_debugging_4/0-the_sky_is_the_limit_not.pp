70039f24d25fe3e244c5e5d8e37d87a0ff41c550
AMRBY <bouyoussefamr@gmail.com>
2024-08-21 14:21:11 +0000

    mandatory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
+#set file limit higher
 
-#0-the_sky_is_the_limit_not.pp set open file limit higher
 exec { 'set limit to 2000':
   path    => '/bin',
   command => "sed -i 's/15/2000/' /etc/default/nginx"
